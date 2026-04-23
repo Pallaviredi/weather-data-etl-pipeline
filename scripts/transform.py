fe5b21c8b8d8056de5e9264fef71ada3001bb0cc
PallaviReddy25 <pallavibobbala19@gmail.com>
2026-04-22 22:39:22 -0500

    Updated transform.py

--- scripts/transform.py
+++ scripts/transform.py
@@ -3,7 +3,7 @@ import pandas as pd
 def transform_data(raw_data):
     transformed = {
         "city": raw_data["name"],
-        "temperature": raw_data["main"]["temp"],
+        "temperature": round(raw_data["main"]["temp"] -273.15,2),
         "humidity": raw_data["main"]["humidity"],
         "weather": raw_data["weather"][0]["description"]
     }
